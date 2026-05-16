f79f07ecea1fdee53f183a8bd2305b61a5e351c9
Sam Alavi <sam.alavi@hotmail.com>
2026-05-16 02:06:04 -0400

    feat: implement asynchronous pipeline execution with scheduled weather data fetching

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from pipeline.extract import fetch_weather, fetch_multiple_locations
+from pipeline.extract import fetch_multiple_locations
 from pipeline.transform import extract_json, create_dataframe
+from pipeline.load import load_parquet, load_raw_data
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
 import logging
 import asyncio
 
@@ -16,8 +21,40 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-response = asyncio.run(fetch_multiple_locations([(12, 12), (0,0)]))
-# print(response)
-weather_obj = extract_json(response)
-df = create_dataframe(weather_obj)
-print(df)
+
+async def run_pipeline(locations: list[tuple]):
+    try:
+        logger.info("── Pipeline run starting ──")
+        response, ingestion_time = await fetch_multiple_locations(locations)
+        load_raw_data(response, ingestion_time)
+        weather_obj = extract_json(response)
+        df = create_dataframe(weather_obj)
+        load_parquet(df)
+        logger.info(f"── Pipeline run complete: {df.shape[0]} rows written ──")
+    except Exception as e:
+        logger.exception(f"Pipeline run failed: {e}")
+
+
+CITIES = [
+    (43.6532,  -79.3832),
+    (35.6892,   51.3890),
+    (34.0522, -118.2437),
+    (49.2827, -123.1207),
+    (45.5017,  -73.5673),
+    (40.7128,  -74.0060),
+]
+
+scheduler = AsyncIOScheduler()
+scheduler.add_job(
+    run_pipeline,
+    trigger=CronTrigger(minute="0,15,30,45"),
+    kwargs={'locations': CITIES}
+)
+
+async def main():
+    scheduler.start()
+    await run_pipeline(CITIES)     
+    await asyncio.Event().wait()
+
+asyncio.run(main())
+
